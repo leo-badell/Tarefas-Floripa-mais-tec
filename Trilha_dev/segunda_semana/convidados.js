f2f2099d544c50dd30e60d96670c5415478ced71
Leonardo Badell <leonardobadell@hotmail.com>
2023-11-06 21:00:40 -0300

    Primeiro commit

---
+++ Trilha_dev/segunda_semana/convidados.js
@@ -0,0 +1,50 @@
+//Tarefa de distribuição dos convidados em setores.
+
+let idade1 = 14;
+let idade2 = 18;
+let idade3 = 25;
+let idade4 = 26;
+
+if (idade1 <= 14) {
+  console.log("Corresponde ao setor amarelo");
+} else if (idade2 < 18) {
+  console.log("Corresponde ao setor azul");
+} else if (idade3 < 25) {
+  console.log("Corresponde ao setor branco");
+} else {
+    console.log("Corresponde ao setor verde");
+}
+
+//Tarefa de oprações aritméticas
+
+let soma = 7 + 7;
+
+console.log(`O resultado da soma 7 + 7 é ${soma}`)
+
+let subtracao = 7 - 5;
+
+console.log(`O resultado da subtração 7 - 5 é ${subtracao}`)
+
+let multiplicacao = 7 * 7;
+
+console.log(`O resultado da multiplicação 7 * 7 é ${multiplicacao}`)
+
+let divisao = 7 / 7;
+
+console.log(`O resultado da divisão 7 / 7 é ${divisao}`)
+
+let modulo = 7 % 7;
+
+console.log(`O resultado do resto 7 % 7 é ${modulo}`)
+
+let expoente = 7 ** 3;
+
+console.log(`O resultado da exponenciação 7 ** 3 é ${expoente}`)
+
+//Concatenação de strings é um importante recurso para a construção de mensagens personalizadas.
+
+const nome = "Leonardo";
+
+const sobrenome = "Badell";
+
+console.log(`Meu nome é ${nome} e meu sobrenome é ${sobrenome}`)
